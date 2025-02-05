b54cf31be9c34d12c772bf65d23902702ff538af
Zach Keatings <zachary.keatings@gmail.com>
2025-02-05 12:00:23 -0500

    Created Basic server with SocketIO
    Navigate to backend/, then run with:
    
    node server.js

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const socketIo = require('socket.io');
+const connectDB = require('./config/db');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+module.exports = io;
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
